5b0330de8fbfb9840946c0232a407eb237d91a5c
Elise Johnsen <elisejohnsen@drift.oslo.kommune.no>
2025-12-14 00:57:40 +0100

    Use the unicorn

--- frontend/app/components/CTA.tsx
+++ frontend/app/components/CTA.tsx
@@ -7,17 +7,17 @@ export default function Example() {
       <div className="">
         <div className="lg:max-w-none">
           <div className="relative">
-            <div className="absolute top-1/2 left-12 -translate-y-1/2 bg-primary-pink p-8 rounded-xl shadow-2xl max-w-md">
+            <div className="absolute top-1/2 left-12 -translate-y-1/4 bg-primary-pink p-8 rounded-xl shadow-2xl max-w-md">
               <p className="text-5xl lg:text-7xl font-semibold tracking-tight text-primary-red leading-tight">
                 Get your hands pearly
               </p>
             </div>
             <Image
               alt="beads"
-              src="/images/beads.jpg"
+              src="/images/unicorn.png"
               width={2432}
               height={1442}
-              className="justify-self-end w-3/4 max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10"
+              className="justify-self-end w-3/4 max-w-none shadow-xl ring-1 ring-gray-400/10"
             />
           </div>
         </div>
