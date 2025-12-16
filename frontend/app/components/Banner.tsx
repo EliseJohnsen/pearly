404c291f847892933d193cfbdd494f0607d4de1f
Elise Johnsen <elisejohnsen@drift.oslo.kommune.no>
2025-12-16 04:49:57 +0100

    add padding

--- frontend/app/components/Banner.tsx
+++ frontend/app/components/Banner.tsx
@@ -2,7 +2,7 @@ import { XMarkIcon } from '@heroicons/react/20/solid'
 
 export default function Banner() {
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-primary-dark-pink px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-primary-dark-pink px-6 py-3">
       <div className="flex flex-auto items-center gap-x-4 gap-y-2">
         <p className="text-sm/6 text-color-primary-red font-bold">
           Inspirasjon
