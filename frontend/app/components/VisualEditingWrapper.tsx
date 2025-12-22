aa79b18b48c7c69c0a78685d52f9f50ac734d954
Elise Johnsen <elisejohnsen@drift.oslo.kommune.no>
2025-12-22 21:59:16 +0100

    add async

--- frontend/app/components/VisualEditingWrapper.tsx
+++ frontend/app/components/VisualEditingWrapper.tsx
@@ -9,7 +9,7 @@ export function VisualEditingWrapper() {
   return (
     <VisualEditing
       portal={true}
-      refresh={(payload) => {
+      refresh={async (payload) => {
         router.refresh();
       }}
     />
